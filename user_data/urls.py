c220d853b655ccadd34f7104c1c84042481f8b6d
cgfennelly <c.g.fennelly@gmail.com>
2022-01-12 21:37:09 +0000

    add partially complete endpoint for returning scores of candidate

--- user_data/urls.py
+++ user_data/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create-candidate/', views.CandidateListCreate.as_view() ),
     path('create-score/', views.ScoreListCreate.as_view() ),
+    path('get-candidate/<slug:candidate_ref>', views.GetScoreListByCandidate.as_view()),
 ]
--- user_data/views.py
+++ user_data/views.py
@@ -2,6 +2,8 @@ from .models import Candidate, Score
 from .serializers import CandidateSerializer, ScoreSerializer
 from rest_framework import generics
 
+from user_data import serializers
+
 # Create your views here.
 class CandidateListCreate(generics.ListCreateAPIView):
     queryset = Candidate.objects.all()
@@ -11,3 +13,11 @@ class CandidateListCreate(generics.ListCreateAPIView):
 class ScoreListCreate(generics.ListCreateAPIView):
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
+
+
+class GetScoreListByCandidate(generics.ListAPIView):
+    serializer_class = ScoreSerializer
+
+    def get_queryset(self):
+        candidate_ref = self.kwargs['candidate_ref']
+        return Score.objects.filter(candidate_ref=candidate_ref)
