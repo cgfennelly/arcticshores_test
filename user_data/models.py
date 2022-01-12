0930d74c058dd45ba580ce1cc5853cabdeccbeb1
cgfennelly <c.g.fennelly@gmail.com>
2022-01-12 17:23:14 +0000

    update field validation for Candidate model

--- user_data/models.py
+++ user_data/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Candidate(models.Model):
     candidate_ref = models.CharField(primary_key=True, max_length=8)
-    name = models.CharField
+    name = models.TextField()
